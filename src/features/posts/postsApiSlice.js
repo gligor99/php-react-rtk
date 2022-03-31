a6147707bb3207ea62f9efcacc097287ed4aae60
gligor99 <lukagligorevic@gmail.com>
2022-03-31 19:52:32 +0200

    Create post done

--- src/features/posts/postsApiSlice.js
+++ src/features/posts/postsApiSlice.js
@@ -5,16 +5,31 @@ export const postsApiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost/php_rest_myblog/api/post",
   }),
+  tagTypes: ["Post"],
   endpoints(builder) {
     return {
       fetchPosts: builder.query({
         query: () => "/read.php",
+        providesTags: ["Post"],
       }),
       fetchSinglePost: builder.query({
         query: (postId) => `/reads_single.php?id=${postId}`,
+      }),
+      addNewPost: builder.mutation({
+        query: (initialPost) => ({
+          url: "/create.php",
+          method: "POST",
+          // We need to include entire post object as the body of the request
+          body: initialPost,
+        }),
+        invalidatesTags: ["Post"],
       }),
     };
   },
 });
 
-export const { useFetchPostsQuery, useFetchSinglePostQuery } = postsApiSlice;
+export const {
+  useFetchPostsQuery,
+  useFetchSinglePostQuery,
+  useAddNewPostMutation,
+} = postsApiSlice;
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAddNewPostMutation } from "../features/posts/postsApiSlice";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
-}
+  const [inputs, setInputs] = useState([]);
+  const [status, setStatus] = useState(undefined);
+  const [addNewPost, { isLoading }] = useAddNewPostMutation();
+  const navigate = useNavigate();
 
-export default CreatePost
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const canSave = [inputs].every(Boolean) && !isLoading;
+
+  const savePost = async () => {
+    if (canSave) {
+      try {
+        await addNewPost(inputs).unwrap();
+        setStatus({ type: "success" });
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      } catch (error) {
+        console.log("Failed to save the post", error);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="p-5 bg-secondary text-center h2 text-white">
+        <span>Create Post</span>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 offset-3">
+            <form>
+              <input
+                className="form-control my-2"
+                type="text"
+                placeholder="Title"
+                name="title"
+                onChange={handleChange}
+                aria-label="default input example"
+              />
+              <input
+                className="form-control my-2"
+                type="text"
+                placeholder="Article"
+                name="body"
+                onChange={handleChange}
+                aria-label="default input example"
+              />
+              <input
+                className="form-control my-2"
+                type="text"
+                placeholder="Author"
+                name="author"
+                onChange={handleChange}
+                aria-label="default input example"
+              />
+              <input
+                className="form-control"
+                type="number"
+                placeholder="Category"
+                name="category_id"
+                onChange={handleChange}
+                aria-label="default input example"
+              />
+              <button
+                className="btn btn-primary form-control my-2"
+                onClick={savePost}
+                disabled={!canSave}
+              >
+                Create post
+              </button>
+            </form>
+            {status?.type === "success" && (
+              <div className="alert alert-warning" role="alert">
+                Post Created
+              </div>
+            )}
+            {status?.type === "error" && (
+              <div className="alert alert-danger" role="alert">
+                Error. Something went wrong. Post is not created!
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreatePost;
